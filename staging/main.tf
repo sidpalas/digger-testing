6cf6f5ca1fcdef1c7350cdb4ba111ac59a6323d7
“Sid <sid@devopsdirective.com>
2025-09-30 09:09:01 -0400

    add bucket

--- staging/main.tf
+++ staging/main.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.14.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo 'Hello World Again!'"
   }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "devops-directivemy-test-bucket"
 }
